bb1cea7642be818db8b34757a15d99bdef554e65
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-23 16:33:15 +0100

    mvp finished

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
-  const input = document.querySelector('#title');
-  input.addEventListener('input', handleInput);
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit); //one event listener for the form submit
 });
 
-const handleInput = function () {
-  const resultParagraph = document.querySelector('#reading-list');
-  resultParagraph.textContent = `${event.target.title.value}`;
+// this 
+const handleFormSubmit = function(event) {
+  event.preventDefault(); //stops page refresh
+
+  const readingList = document.querySelector('#reading-list'); 
+  const newItem = document.createElement('li'); //creates new li element 
+  newItem.textContent = `${event.target.title.value}, ${event.target.author.value},
+  ${event.target.category.value}`;
+  
+  //gives it a class of item
+  newItem.classList.add('item'); 
+  
+
+  readingList.appendChild(newItem);
+
+  //resets after each submission
+  document.getElementById('new-item-form').reset();
 }
+
+
